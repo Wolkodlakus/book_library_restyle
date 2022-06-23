d7e9f3a03fc55fb270b47cd9bb7ef31ff2db9e38
Wolkodlakus <lesko68@list.ru>
2022-06-23 14:58:22 +0300

    Add parser arguments

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,12 +2,12 @@
 """
 Скрипт для скачивания книг из библиотеки tululu.org
 """
+import argparse
 import json
-
-from urllib.parse import urljoin, urlsplit, unquote
-
 import requests
+
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlsplit, unquote
 
 from parse_tululu import parse_book_page, download_txt,  download_image
 
@@ -17,9 +17,20 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def parse_category_pages(url_category, page_count):
+def get_max_page_number(url):
+    params = ''
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    check_for_redirect(response)
+    soup = BeautifulSoup(response.text, 'lxml')
+    return int(soup.select_one('.npage:last-child').text)
+
+
+def parse_category_pages(url_category, start_page, end_page):
     book_pages = []
-    for number_page in range(1, page_count+1):
+    if not end_page:
+        end_page = get_max_page_number(url_category)
+    for number_page in range(start_page, end_page+1):
         if number_page > 1:
             url_page = f'{url_category}/{number_page}'
         else:
@@ -40,15 +51,27 @@ def parse_category_pages(url_category, page_count):
     return book_pages
 
 
+def create_args_parser():
+    parser = argparse.ArgumentParser(description='Программа для скачивания книг с .. по ..')
+    parser.add_argument('start_page', nargs='?', default='1', help='С какой страницы начинать', type=int)
+    parser.add_argument('end_page', nargs='?', default='0', help='Какой страницей заканчивать', type=int)
+    return parser
+
+
 def main():
+    parser = create_args_parser()
+    args = parser.parse_args()
+    start = args.start_page
+    end = args.end_page
 
     book_folder = 'books'
     url_txt = 'https://tululu.org/txt.php'
     url_scifi_cat = 'https://tululu.org/l55/'
-
-    print(f'Программа начинает скачивание книг SciFi по 4 страницу')
-    book_pages = parse_category_pages(url_scifi_cat, 4)
-    print(book_pages)
+    if end:
+        print(f'Программа начинает скачивание книг SciFi с {start} по {end} страницу')
+    else:
+        print(f'Программа начинает скачивание книг SciFi с {start} страницы до конца')
+    book_pages = parse_category_pages(url_scifi_cat, start, end)
     books_properties = []
     for book_page in book_pages:
 
